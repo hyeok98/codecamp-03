3a752589e22abd07e5ca7bc3d07677f9fc3be9f0
hyeokangel <dltkdgur7171@gmail.com>
2021-12-02 19:52:40 +0900

    tst

--- freeboard_frontend/src/components/commons/layout/navigation/LayoutNavigation.styles.ts
+++ freeboard_frontend/src/components/commons/layout/navigation/LayoutNavigation.styles.ts
@@ -7,7 +7,7 @@ export const Wrapper = styled.div`
   background-color: white;
   width: 100%;
   height: 100px;
-  margin-bottom: 30px;
+  margin-bottom: 19px;
 `;
 
 export const Main = styled.div`
